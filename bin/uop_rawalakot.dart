13f91540853be13c0c479b67836b74485d908206
Abid <abidroid1@gmail.com>
2025-04-24 10:16:01 +0500

    relation operator

--- bin/uop_rawalakot.dart
+++ bin/uop_rawalakot.dart
@@ -1,30 +1,5 @@
 void main(){
-  print('welcome to Dart');
-  print("yes");
 
-  int totalStudents;
-  totalStudents = 40;
-  print(totalStudents);
-
-  double gpa = 3.3;
-  print(gpa);
-
-  String name = 'Abid';
-
-  bool isVaccinated = true;
-  bool isMarried = true;
-
-  // Type inference
-  var course = "Mob app dev";
-  print(course.runtimeType);
-
-  dynamic variable;
-  variable = 123;
-  variable = true;
-  variable = "Ali";
-
-  final double pi = 3.14;
-
-  
+  print(5 > 2);
 
 }
