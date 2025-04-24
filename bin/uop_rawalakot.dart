b63554915fdba580e90916623996b78b0878eebe
Abid <abidroid1@gmail.com>
2025-04-24 18:19:28 +0500

    inheritance and method overriding

--- bin/uop_rawalakot.dart
+++ bin/uop_rawalakot.dart
@@ -1,36 +1,108 @@
-class Doctor {
+class Person {
+  String name;
+  String dob;
+  String gender;
 
-  late String name ;
+  Person({required this.name, required this.dob, required this.gender});
+
+  void display() {
+    print('Name $name');
+    print('DOB: $dob');
+    print('Gender: $gender');
+  }
+}
+
+class Beggar extends Person {
+  Address address;
+  int dailyIncome;
+
+  Beggar({
+    required String name,
+    required String dob,
+    required String gender,
+    required this.address,
+    required this.dailyIncome,
+  }) : super(name: name, dob: dob, gender: gender);
+
+  void display() {
+    super.display();
+    print('Location: ${address.city}');
+    print('Income: $dailyIncome');
+  }
+}
+
+void main() {
+  Beggar ali = Beggar(
+    name: 'Ali',
+    dob: '1st April',
+    gender: 'M',
+    address: Address(
+      houseNum: '2',
+      streetNum: '4',
+      city: 'RWK',
+      country: 'PAK',
+    ),
+    dailyIncome: 5000,
+  );
+  ali.display();
+}
+
+class Address {
+  String houseNum;
+  String streetNum;
+  String city;
+  String country;
+
+  Address({
+    required this.houseNum,
+    required this.streetNum,
+    required this.city,
+    required this.country,
+  });
+}
+
+class Doctor {
+  late String name;
   late String spe;
   late int fee;
   late String clinicAddress;
+  String? contactNum;
 
-  // default constructor
-
-  // Parameterized
-  Doctor(String n, String s, int f, String add) {
-    name = n;
-    spe = s;
-    fee = f;
-    clinicAddress = add;
-  }
+  // Parameterized constructor
+  Doctor({
+    required this.name,
+    required this.spe,
+    required this.fee,
+    required this.clinicAddress,
+    this.contactNum,
+  });
 
   void display() {
     print('Name $name');
     print('Spe: $spe');
     print('Fee: $fee');
     print('Address: $clinicAddress');
+    print('Contact: $contactNum');
   }
 }
 
-void main(){
+class Student {
+  String name;
+  String course;
+  String? homeAddress;
+  String? contactNum;
 
-  Doctor doc1 = Doctor( "Bilal", "Gastro", 2500, "Baldia Ada");
-  doc1.display();
+  Student({
+    required this.name,
+    required this.course,
+    this.homeAddress,
+    this.contactNum,
+  });
 
-
-  Doctor doc2 = Doctor( "Zia", "Ent", 3500, "Rawalakot STC");
-
+  void display() {
+    print('Name $name');
+    print('Course: $course');
+    print('HomeADDress: $homeAddress');
+    print('Contact: $contactNum');
+  }
 }
-
-
