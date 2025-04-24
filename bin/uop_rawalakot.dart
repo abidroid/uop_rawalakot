297522ad4952464f17fb4e89ca30ea32738c6f4b
Abid <abidroid1@gmail.com>
2025-04-24 10:27:27 +0500

    operator, interpolation, null safety

--- bin/uop_rawalakot.dart
+++ bin/uop_rawalakot.dart
@@ -1,5 +1,25 @@
 void main(){
 
-  print(5 > 2);
+  print(5 + 2);
+  print(5 - 2);
+  print(5 * 2);
+   print(5 / 2);
+   print(5.0 ~/ 2);
+   print(5 % 2);
 
+   String firstName = "Ali";
+   String lastName = "Khan";
+
+   // string interpolation
+   String fullName = "$firstName $lastName";
+
+   print(fullName);
+
+   int age = 50;
+   print( 'My age is $age' );
+   print('next year i will be ${age+1} years old');
+
+   String? fianceName;
+
+   print(fianceName);
 }
